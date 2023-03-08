abdd12905805a2d98fe6604af949f47a0c8154a9
mehak <mehakdhawan5994@gmail.com>
2023-03-08 19:42:41 +0530

    --c2 company logo--

---
+++ qtify/src/components/Logo/Logo.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+function Logo() {
+    return (
+        <img src={require("../../assets/qtifylogo.png")} alt="brand logo" width={67} />
+    )
+}
+
+export default Logo
